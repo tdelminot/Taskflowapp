3dc1169321b760d3ae8143bcaee3415465323392
Tafar delminot <tdelmont@gmail.com>
2026-06-09 17:49:00 +0200

    correction du serveur qui restait allumé après les tests

--- src/app.js
+++ src/app.js
@@ -77,9 +77,16 @@ async function startServer() {
             console.log('✅ Database synced');
         }
         
-        app.listen(PORT, () => {
+       /* app.listen(PORT, () => {
             console.log(`🚀 Server running on port ${PORT}`);
-        });
+        }); */
+if (require.main === module) {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on port ${PORT}`);
+    });
+}
+
     } catch (error) {
         console.error('❌ Unable to connect to database:', error);
         process.exit(1);
